ae4f0d8d4aaa2b9c4b2cb78974718b05d0d13530
crystel1996 <crystelratsimbazafy@gmail.com>
2023-08-10 20:03:04 +0300

    add style layout logo

--- front/src/Components/Sidebar/Sidebar.tsx
+++ front/src/Components/Sidebar/Sidebar.tsx
@@ -1,13 +1,15 @@
 import React, { FC } from 'react';
 import { SidebarComponentInterface } from './interface';
 import { Logo } from '../Logo';
+import style from './style.module.css';
 
 export const Sidebar: FC<SidebarComponentInterface> = () => {
 
-    return <>
+    return <div className={style.sidebar}>
+        <div className={style.sidebarLogo}>
+            <Logo />
+        </div>
     
-        <Logo />
-    
-    </>
+    </div>
 
 }
