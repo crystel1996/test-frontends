210135768f6d8dd5f2f7dd49e427f38d849c0ec4
crystel1996 <crystelratsimbazafy@gmail.com>
2023-08-10 19:57:35 +0300

    add style logo layout

--- front/src/Components/Logo/Logo.tsx
+++ front/src/Components/Logo/Logo.tsx
@@ -1,10 +1,11 @@
 import React, { FC } from 'react';
 import { LogoComponentInterface } from './interface';
 import { LogoImage } from './LogoImage';
+import style from './style.module.css';
 
 export const Logo: FC<LogoComponentInterface> = () => {
 
-    return <div>
+    return <div className={style.logoLayout}>
         <LogoImage />
     </div>
 
