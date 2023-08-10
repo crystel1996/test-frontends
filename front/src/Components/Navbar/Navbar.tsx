42542e4d9a5ad2ef4d5fd38c7350b11b562467ae
crystel1996 <crystelratsimbazafy@gmail.com>
2023-08-10 20:06:14 +0300

    add navbar component

---
+++ front/src/Components/Navbar/Navbar.tsx
@@ -0,0 +1,11 @@
+import React, { FC } from 'react';
+import { NavbarComponentInterface } from './interface';
+
+
+export const Navbar: FC<NavbarComponentInterface> = () => {
+
+    return <div>
+        
+    </div>
+
+}
--- front/src/Components/Sidebar/Sidebar.tsx
+++ front/src/Components/Sidebar/Sidebar.tsx
@@ -2,14 +2,18 @@ import React, { FC } from 'react';
 import { SidebarComponentInterface } from './interface';
 import { Logo } from '../Logo';
 import style from './style.module.css';
+import { Navbar } from '../Navbar';
 
 export const Sidebar: FC<SidebarComponentInterface> = () => {
 
     return <div className={style.sidebar}>
+        
         <div className={style.sidebarLogo}>
             <Logo />
         </div>
-    
+
+        <Navbar />
+
     </div>
 
 }
