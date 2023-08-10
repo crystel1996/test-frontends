73e1c09391dff134ab65a3a8b4d7827ef1d6be52
crystel1996 <crystelratsimbazafy@gmail.com>
2023-08-10 20:08:40 +0300

    navbar style layout

--- front/src/Components/Navbar/Navbar.tsx
+++ front/src/Components/Navbar/Navbar.tsx
@@ -1,10 +1,11 @@
 import React, { FC } from 'react';
 import { NavbarComponentInterface } from './interface';
+import style from './style.module.css';
 
 
 export const Navbar: FC<NavbarComponentInterface> = () => {
 
-    return <div>
+    return <div className={style.navbar}>
         
     </div>
 
